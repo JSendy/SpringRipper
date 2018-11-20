f91ddd9ad4ee98722d8c02b4a404f141a7ffd707
Rendy <mrsendyuk@gmail.com>
2018-11-21 00:07:40 +0200

    init

---
+++ src/main/java/quoters/InjectRandomIntAnnotationBeanPostProcessor.java
@@ -0,0 +1,31 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+public class InjectRandomIntAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        Field[] fields = o.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+            if(annotation != null){
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int i = min + random.nextInt(max - min);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field,o,i);
+            }
+        }
+        return o;
+    }
+
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        return o;
+    }
+}
